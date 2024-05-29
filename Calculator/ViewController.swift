d94f54a62987a7fdda32051dc058a505fc48dbc3
nasoviva <nasoviva@gmail.com>
2024-05-28 22:36:12 -0300

    add outlets and actions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func dotPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func equalsPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func operationPressed(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 

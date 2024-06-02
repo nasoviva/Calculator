7f462b15d6d92cc68dcffafe7e047d41e2050ae4
nasoviva <nasoviva@gmail.com>
2024-06-01 22:20:25 -0300

    fix code

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,26 +11,39 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var label: UILabel!
-    
+
+
     var numberIsTyping: Double = 0
     var firstOperand: Double = 0
     var operationSign: Bool = false
     var operationTag: Int = 0
-    
+    var result: String = ""
+
+
     @IBAction func clearPressed(_ sender: UIButton) {
         if sender.tag == 17 {
-            label.text = ""
-            firstOperand = 0
-            numberIsTyping = 0
-            operationTag = 0
-            operationSign = false
+            clearAll()
         }
-        else if sender.tag == 15 {
+        else if sender.tag == 15 && label.text!.isEmpty == false {
             label.text!.removeLast()
         }
     }
-    
+
+    func clear () {
+        firstOperand = 0
+        numberIsTyping = 0
+        operationTag = 0
+        operationSign = false
+       // result = ""
+    }
+
+    func clearAll() {
+        clear()
+        label.text = "0"
+    }
+
     @IBAction func equalsPressed(_ sender: UIButton) {
+      //  print(result)
         if operationTag == 11 {
             printResult(firstOperand + numberIsTyping)
         }
@@ -48,39 +61,53 @@ class ViewController: UIViewController {
                 printResult(firstOperand / numberIsTyping)
             }
         }
-        firstOperand = 0
-        numberIsTyping = 0
-        operationTag = 0
-        operationSign = false
+        clear()
     }
     
     func printResult(_ number: Double) {
-        if number == floor(number) {
-            label.text = String(format: "%.0f", number)
-        } else {
-            label.text = String(number)
+        if number >= (0 - 99999999999999) && number <= 999999999999999 {
+            if number == floor(number) {
+                label.text = String(format: "%.0f", number)
+            } else {
+                label.text = String(number)
+            }
+        }
+        else {
+            label.text = "Err"
         }
     }
-    
+
     @IBAction func numberPressed(_ sender: UIButton) {
-        var num = String(sender.tag)
-        if sender.tag == 16 {
-            num = "."
+        if label.text!.count < 15 {
+            let num = checkDot(sender.tag)
+          //  result = result + num
+            if operationSign == true {
+                label.text = num
+                operationSign = false
+            }
+            else if operationSign == false && label.text != "0" {
+                label.text = label.text! + num
+            }
+            else  if operationSign == false && label.text == "0" {
+                label.text = num
+            }
+            numberIsTyping = Double(label.text!)!
         }
-        if operationSign == true {
-            label.text = num
-            operationSign = false
-        }
-        else if operationSign == false && firstOperand != 0 {
-            label.text = label.text! + num
-        } else {
-            label.text = num
-        }
-        numberIsTyping = Double(label.text!)!
     }
-    
+
+    func checkDot(_ tag: Int) -> String {
+        if (label.text == "0" || label.text == "" || label.text == "+" || label.text == "-" || label.text == "*" || label.text == "/") && tag == 16 {
+            return "0."
+        } else if label.text!.contains(".") == false && tag == 16 {
+           return "."
+        } else if tag == 16 {
+            return ""
+        }
+        return String(tag);
+    }
+
     @IBAction func operationPressed(_ sender: UIButton) {
-        if label.text != "" {
+        if operationSign != true && label.text != "-" {
             firstOperand = Double(label.text!)!
             if sender.tag == 11 {
                 label.text = "+"
@@ -89,11 +116,12 @@ class ViewController: UIViewController {
                 label.text = "-"
             }
             else if sender.tag == 13 {
-                label.text = "x"
+                label.text = "*"
             }
             else if sender.tag == 14 {
                 label.text = "/"
             }
+           // result = result + label.text!
             operationTag = sender.tag
             operationSign = true
         }

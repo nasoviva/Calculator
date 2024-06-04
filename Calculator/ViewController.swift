494906d8d45d56edd45c3c69b59f9118bda9fcfb
nasoviva <nasoviva@gmail.com>
2024-06-03 22:33:30 -0300

    test and check code

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,18 +7,16 @@
 
 import UIKit
 import Expression
+import Foundation
 
 class ViewController: UIViewController {
-    
-    
-    @IBOutlet weak var label: UILabel!
 
+    @IBOutlet weak var label: UILabel!
 
     var numberIsTyping: Double = 0
     var firstOperand: Double = 0
     var operationSign: Bool = false
     var operationEquels: Bool = false
-    var operationTag: Int = 0
     var result: String = ""
 
 
@@ -26,54 +24,76 @@ class ViewController: UIViewController {
         if sender.tag == 15 {
             clearAll()
         }
-//        else if sender.tag == 15 && label.text!.isEmpty == false {
-//            label.text!.removeLast()
-//        }
+        else if sender.tag == 17 && label.text != "Err" {
+            if label.text!.count > 1 {
+                label.text!.removeLast()
+            }
+            else if label.text!.count <= 1 {
+                label.text = "0"
+            }
+        }
+    }
+
+    func clear() {
+        firstOperand = 0
+        numberIsTyping = 0
+        operationSign = false
+        result = ""
     }
 
     func clearAll() {
-        firstOperand = 0
-        numberIsTyping = 0
-        operationTag = 0
-        operationSign = false
+        clear()
         operationEquels = false
         label.text = "0"
-
     }
 
     @IBAction func equalsPressed(_ sender: UIButton) {
         operationEquels = true
-        if operationSign == true && (label.text == "0" || label.text == "0." || label.text == "" || label.text == "+" || label.text == "-" || label.text == "*" || label.text == "/") {
+        if operationSign == true && (label.text == "0" || label.text == "0." || label.text == "" || label.text == "-"){
             numberIsTyping = 0
-
         }
         result = result + " " + String(numberIsTyping)
-       //print("equalsPressed result= " + result)
+        getResult()
+        clear()
+    }
+
+    func getResult() {
         let expressionString = result
         let expression = Expression(expressionString)
         var finalResult: Double = 0
         do {
             finalResult = try expression.evaluate()
-        } catch {
-            print("Failed to evaluate expression: \(error)")
-        }
-        printResult(finalResult)
-        firstOperand = 0
-        numberIsTyping = 0
-        operationTag = 0
-        operationSign = false
-        result = ""
-    }
-    
-    func printResult(_ number: Double) {
-        if number >= (0 - 99999999999999) && number <= 999999999999999 {
-            if number == floor(number) {
-                label.text = String(format: "%.0f", number)
+            if finalResult.isNormal {
+                printResult(Decimal(finalResult))
             } else {
-                label.text = String(number)
+                label.text = "Err"
             }
+        } catch {
+           print("Err")
         }
+    }
 
+    func printResult(_ number: Decimal) {
+        if number >= (0 - 99999999999999) && number <= 999999999999999 {
+            formatResult(number)
+        }
+        else {
+            label.text = "Err"
+        }
+    }
+
+    func formatResult(_ number: Decimal) {
+
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 13 // number of digits after the decimal point
+        formatter.decimalSeparator = "." // dot as a separator
+        formatter.groupingSeparator = "" // thousands separator
+
+        if let formattedString = formatter.string(from: number as NSDecimalNumber) {
+            let truncatedString = String(formattedString.prefix(15))
+            label.text = String(truncatedString)
+        }
         else {
             label.text = "Err"
         }
@@ -97,7 +117,7 @@ class ViewController: UIViewController {
             else if operationSign == false && label.text != "0" {
                 label.text = label.text! + String(sender.tag)
             }
-            else  if operationSign == false && label.text == "0" {
+            else if operationSign == false && label.text == "0" {
                 label.text = String(sender.tag)
             }
         }
@@ -105,7 +125,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func operationPressed(_ sender: UIButton) {
-        if operationSign != true {//&& label.text != "-"
+        if operationSign != true && label.text != "-" {
             if label.text == "0" || label.text == "0." || label.text == "" || label.text == "+" || label.text == "-" || label.text == "*" || label.text == "/" {
                 firstOperand = 0
             }
@@ -118,21 +138,18 @@ class ViewController: UIViewController {
                 result = result + " " + String(firstOperand)
             }
             if sender.tag == 11 {
-                label.text = "+"
+                result = result + " " + "+"
             }
             else if sender.tag == 12 {
-                label.text = "-"
+                result = result + " " + "-"
             }
             else if sender.tag == 13 {
-                label.text = "*"
+                result = result + " " + "*"
             }
             else if sender.tag == 14 {
-                label.text = "/"
+                result = result + " " + "/"
             }
-            operationTag = sender.tag
-            result = result + " " + label.text!
             operationSign = true
-//           print("operationPressed result = " + result)
         }
      }
 }
